1af7cc3aa615d2f2245e6adeb369c2a9e71a7f5e
Hyun Don Moon <hyundonmoon@gmail.com>
2022-02-28 23:17:30 +0900

    modify: made active  slider item scroll into view

--- components/slider/slider-item/index.tsx
+++ components/slider/slider-item/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import classNames from 'classnames/bind';
 import styles from '@/components/slider/slider-item/styles.module.scss';
 import Link from 'next/link';
@@ -11,13 +12,22 @@ interface SliderItemProps {
 }
 
 const SliderItem = ({ name, id, params }: SliderItemProps) => {
+  const itemRef = useRef<HTMLAnchorElement>(null);
+
+  useEffect(() => {
+    if (itemRef.current && params === id) {
+      itemRef.current.scrollIntoView();
+    }
+  });
+
   return (
     <Link href={`/categories/${id}`}>
       <a
         className={cx({
           'slider-item': true,
           active: params === id,
-        })}>
+        })}
+        ref={itemRef}>
         {name}
       </a>
     </Link>
